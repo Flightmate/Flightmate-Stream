c5417080d645a5d802234ef676b3db0294e0125a
Hampus Dunström <hampus@dunstrom.com>
2018-02-09 11:22:50 +0100

    Updated client with SSL connection.

--- NetProtocol.py
+++ NetProtocol.py
@@ -1,104 +1,126 @@
 import struct
+import logging
 from binascii import crc32
-from datetime import datetime
 
-DATE_TIME_FORMAT = '%Y-%m-%d, %H:%M:%S'
 
-def time_print(message):
-	print("[{}]: {}".format(datetime.now().strftime(DATE_TIME_FORMAT), message))
+class StreamError(Exception):
+	pass
+
 
 class NetProtocol(object):
 	"""
 	This class pack/unpack data that will be sent from and to FlightmateStream
 	"""
+
 	def __init__(self):
 		super(NetProtocol, self).__init__()
+		self.HEADER_FORMAT = '!LLB'
+		self.PRE_HEADER_FORMAT = '!LB'
+		self.HEADER_SIZE = 9
+		self.MAX_BODY_SIZE = 2000 * 1024
+		self.MIN_BODY_SIZE = 1
+		self.ENCODING = 'utf-8'
+		self.READ_SIZE = 4096
+		self.BAD_PACKET = -1
+		self.SEARCH_PACKET = 1
+		self.CLICK_PACKET = 2
+		self.OTA_AUTH_PACKET = 3
+		self.MASKED_PACKET = 4
+		self.POSTBOY_AUTH_PACKET = 5
 
-		self.headerFormat = '!LLB'
-		self.preHeaderFormat = '!LB'
-		self.headerSize = 9
-		self.maxBodySize = 2000 * 1024
-		self.minBodySize = 1
-		self.encoding = 'utf-8'
-		self.readSize = 4096
-		self.SEARCHPACKET = 1
-		self.CLICKPACKET = 2
-		self.AUTHPACKET = 3
-		self.BADPACKET = -1
-	
-	def readSocket(self, sock, size, readSize):
+	@staticmethod
+	def _read_socket(sock, size, read_size):
 		"""
-		Reads a certain amount of data from a socket. This method does not close 
-		the socket but returns None if the socket breaks before reading the 
+		Reads a certain amount of data from a socket. This method does not close
+		the socket but returns None if the socket breaks before reading the
 		specified amount of data.
-		:param socket sock: The socket to read from.
+		:param socket.Socket sock: The socket to read from.
 		:param int size: The amount of bytes to read from the socket.
+		:param int read_size: The total amount of bytes to read.
 		:return binary: The data read if the specified amount of data was succesfully read.
 		"""
-		totRead = 0
+		tot_read = 0
 		chunks = []
-		while totRead < size:
-			sizeLeft = size - totRead
-			if sizeLeft < readSize:
-				readSize = sizeLeft
+		while tot_read < size:
+			size_left = size - tot_read
+			if size_left < read_size:
+				read_size = size_left
 			try:
-				chunk = sock.recv(readSize)
-			except:
-				time_print("Error receivning chunk")
-				return None
-			chunkSize = len(chunk)
-			if chunkSize == 0:
+				chunk = sock.recv(read_size)
+			except Exception:
+				logging.exception("Error receiving chunk")
+				raise
+			chunk_size = len(chunk)
+			if chunk_size == 0:
 				raise BrokenPipeError("The socket connection has been broken.")
-			totRead += chunkSize
+			tot_read += chunk_size
 			chunks.append(chunk)
 		data = b''.join(chunks)
 		if len(data) == size:
 			return data
 		else:
-			raise ValueError("data read not of correct lengt. Length is %d and should be %d" % (len(data), size))
-	
-	def readHeader(self, sock):
+			raise ValueError("Data read not of correct length. Length is %d and should be %d" % (len(data), size))
+
+	def _read_header(self, sock):
 		"""
 		Reads the header of a packet and returns a list of it's content.
 		:param socket sock: The socket to read the header from.
 		:return list: Returns a list [(int) checksum, (int) bodySize, (int) dataType]. If something goes wrong
 		a empty list is returned.
 		"""
-		try:
-			data = self.readSocket(sock, self.headerSize, self.headerSize)
-		except (BrokenPipeError, ValueError) as e:
-			time_print(e)
-			return []
+		data = self._read_socket(sock, self.HEADER_SIZE, self.HEADER_SIZE)
 		if data:
-			header = struct.unpack(self.headerFormat, data)
+			header = struct.unpack(self.HEADER_FORMAT, data)
 		else:
-			return []
+			raise StreamError("No data to unpack while trying to read the packet header.")
 
 		# Control header
 		if len(header) == 3:
 			return header
-		return []
+		raise StreamError("Unpacked header of length %d instead of 3", len(header))
 
-	def generatePacket(self, requestType, requestBody):
+	def _generate_packet(self, request_type, request_body):
 		"""
 		Creates a header and uses it to generate a packet.
-		:param int requestType: The type of the request representet by a int.
-		:param binary requestBody: The data you want to pack.
+		:param int request_type: The type of the request representet by a int.
+		:param binary request_body: The data you want to pack.
 		"""
-		bodyLength = len(requestBody)
-		preHeader = struct.pack(self.preHeaderFormat, bodyLength, requestType)
-		message = preHeader + requestBody
+		body_length = len(request_body)
+		pre_header = struct.pack(self.PRE_HEADER_FORMAT, body_length, request_type)
+		message = pre_header + request_body
 		checksum = crc32(message)
-		header = struct.pack(self.headerFormat, checksum, bodyLength, requestType)
-		return header + requestBody
+		header = struct.pack(self.HEADER_FORMAT, checksum, body_length, request_type)
+		return header + request_body
 
-	def controlChecksum(self, header, body):
+	def _control_checksum(self, header, body):
 		"""
-		Controles the checksum of a message.
-		:param list header: The header of the message to control. This contains the checksum to check against.
+		Controls the checksum of a message.
+		:param tuple header: The header of the message to control. This contains the checksum to check against.
 		:param binary body: The body of the message to contorl.
 		:return bool: True if the checksums matches otherwise false.
 		"""
 		checksum = header[0]
-		message = struct.pack(self.preHeaderFormat, header[1], header[2]) + body
+		message = struct.pack(self.PRE_HEADER_FORMAT, header[1], header[2]) + body
 		return crc32(message) == checksum
+
+	def _read_packet(self, sock):
+		"""
+		Reads a packet from a socket and returns the packet body and it's type. Raises a StreamError upon failure.
+		:param socket sock: The socket to read the packet from.
+		:return (int, string): The type of the packet and it's content.
+		"""
+		# Read the header.
+		header = self._read_header(sock)
+		# Control body size
+		body_size = header[1]
+		if body_size > self.MAX_BODY_SIZE or body_size < self.MIN_BODY_SIZE:
+			raise StreamError(
+				"Body size %d, limit is max %d and min %d" % (body_size, self.MAX_BODY_SIZE, self.MIN_BODY_SIZE))
+
+		body = self._read_socket(sock, body_size, self.READ_SIZE)
+
+		if not self._control_checksum(header, body):
+			raise StreamError("Checksum failed.")
+
+		packet_type = header[2]
+		return packet_type, body
--- exampleClient.py
+++ exampleClient.py
@@ -1,59 +1,71 @@
 #!/usr/bin/env python3.6
-import socket, select, time, json
+import socket
+import select
+import json
+import ssl
+import sys
 from NetProtocol import NetProtocol
+
 
 class ExampleClient(NetProtocol):
 
 	def __init__(self):
 		super().__init__()
 		self.token = 'INSERT YOUR TOKEN HERE'
+		if self.token == 'INSERT YOUR TOKEN HERE':
+			print("You must insert your token in exampleClient.py for the client to be able to connect to the server.")
+			sys.exit()
 		self.host = 'ai-stream.flightmate.com'
-		self.port = 443
+		self.port = 444
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.settimeout(20)
+		self.context = ssl.create_default_context()
+		self.sock = self.context.wrap_socket(self.sock, server_hostname=self.host)
 
 	def _connect(self):
-		packet = self.generatePacket(3, self.token.encode('utf-8'))
+		packet = self._generate_packet(3, self.token.encode('utf-8'))
 		self.sock.connect((self.host, self.port))
 		self.sock.sendall(packet)
 
-	def _handle_search_packet(self, message):
-		print("Got a search packet")
+	@staticmethod
+	def _handle_search_packet(message):
+		print("Got a packet containing a search from %s to %s from %s." % (message['from'], message['to'], message['domain']))
 
-	def _handle_click_packet(self, message):
-		print("Got a click packet")
+	@staticmethod
+	def _handle_click_packet(message):
+		print("Got a click packet from %s" % message['domain'])
 
 	def run(self):
 		self._connect()
 		while True:
 			readables, writable, exceptional = select.select([self.sock], [], [])
 			for readable in readables:
-				header = self.readHeader(readable)
+				header = self._read_header(readable)
 				if not header:
 					print("Failed to read header.")
 					readable.close()
 					continue
 				
 				# Control body size
-				bodySize = header[1]
-				packetType = header[2]
-				if bodySize > self.maxBodySize or bodySize < self.minBodySize:
-					print("Body size %d, limit is max %d and min %d" % (bodySize, self.maxBodySize, self.minBodySize))
+				body_size = header[1]
+				packet_type = header[2]
+				if body_size > self.MAX_BODY_SIZE or body_size < self.MIN_BODY_SIZE:
+					print("Body size %d, limit is max %d and min %d" % (body_size, self.MAX_BODY_SIZE, self.MIN_BODY_SIZE))
 					readable.close()
 					continue
 				
-				body = self.readSocket(readable, bodySize, self.readSize)
+				body = self._read_socket(readable, body_size, self.READ_SIZE)
 
-				if not self.controlChecksum(header, body):
+				if not self._control_checksum(header, body):
 					print("Checksum failed.")
 					readable.close()
 					continue
 
-				print("Received packet of type: %s" % packetType)
+				print("Received packet of type: %s" % packet_type)
 				message = json.loads(body.decode('utf-8'))
-				if packetType == 1:
+				if packet_type == 1:
 					self._handle_search_packet(message)
-				elif packetType == 2:
+				elif packet_type == 2:
 					self._handle_click_packet(message)
 					
 
