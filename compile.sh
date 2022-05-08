5b5cb04565cf9e3f6e457727b002d38cc430d046
MazeOfIth <76459089+MazeofIth@users.noreply.github.com>
2022-05-08 21:56:19 +0200

    go 1.18 dropped support of darwin 386 (mostly 32 bit macOS systems)

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 # assign execute permissions with chmod +x compile.sh before running with ./compile.sh
-env GOOS=windows  GOARCH=386 go build -o executables/streamclient-windows-386.exe client.go &
-env GOOS=windows  GOARCH=amd64 go build -o executables/streamclient-windows.exe client.go &
-env GOOS=darwin GOARCH=386 go build -o executables/streamclient-macOS-386 client.go & 
+env GOOS=windows GOARCH=386 go build -o executables/streamclient-windows-386.exe client.go &
+env GOOS=windows GOARCH=amd64 go build -o executables/streamclient-windows.exe client.go &
 env GOOS=darwin GOARCH=amd64 go build -o executables/streamclient-macOS client.go &
 env GOOS=linux GOARCH=386 go build -o executables/streamclient-linux-386 client.go & 
 env GOOS=linux GOARCH=amd64 go build -o executables/streamclient-linux client.go
